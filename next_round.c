afa0707cebc043912b5fa43af3f69c9a35dbf3c8
RubabaBinteMahbub <rubababintemahbub2007@gmail.com>
2025-10-15 18:21:34 +0600

    next_round.c

--- next_round.c
+++ next_round.c
@@ -1,1 +1,18 @@
+#include<stdio.h>
+int main(){
+int n,k,count=0;
+int a[100];
+scanf("%d%d",&n,&k);
 
+for(int i=0;i<n;i++){
+scanf("%d",&a[i]);
+}
+
+for(int i=0;i<n;i++){
+if(a[i]!=0 && a[i]>=a[k-1]){
+count++;
+}
+}
+printf("%d\n",count);
+return 0;
+}
