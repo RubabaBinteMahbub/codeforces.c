ff4572ee41572d5c0ec36b4afe63c9a8b6621744
RubabaBinteMahbub <rubababintemahbub2007@gmail.com>
2025-10-16 12:17:48 +0600

    boy_or_girl.cpp to evaluate gender response
    Implement a program to determine gender response based on unique character count.

---
+++ boy_or_girl.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+using namespace std;
+int main()
+{
+    string s;
+    int count=0;
+    cin>>s;
+    sort(s.begin(),s.end()); 
+
+//Algorithm header file.
+    
+for(int i=0;i<s.size();i++){
+        if(s[i]!=s[i+1]) count++;
+    }
+    if(count%2==0) cout<<"CHAT WITH HER!\n";
+    else cout<<"IGNORE HIM!\n";
+}
